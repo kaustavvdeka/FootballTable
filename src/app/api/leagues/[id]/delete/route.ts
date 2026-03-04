6a16262f17aa712013cf31643ba2e41dd0f1ce43
kaustavvdeka <chwapnamkashyap@gmail.com>
2026-03-04 23:37:28 +0530

    Production

--- src/app/api/leagues/[id]/delete/route.ts
+++ src/app/api/leagues/[id]/delete/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
-
+export const dynamic = "force-dynamic";
 export async function DELETE(
     request: Request,
     { params }: { params: Promise<{ id: string }> }
